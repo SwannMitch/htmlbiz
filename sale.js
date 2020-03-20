c37d53434fce7962271f9b675aa8747c64b6bf6f
SwannMitch <swann_m@rocketmail.com>
2020-03-20 13:32:58 -0700

    fixed for and while loops

--- sale.js
+++ sale.js
@@ -26,27 +26,36 @@ function mailinglist(){
     var email = prompt('Please join our mailing list for exclusive deals!', 'valuedcustomer@mail.com')
     var reply;
 
-        if (email === 'null') {
+    if (email === 'null') {
         reply = 'Maybe next time.'; 
-    } else if (email === '') {
-    reply = 'Maybe next time.'; 
-    }    else {
-    reply = 'Thank you!';
+    } 
+    else if (email === '') {
+        reply = 'Maybe next time.'; 
+    }    
+    else {
+        reply = 'Thank you!';
     }
 
-alert(reply)
+alert(reply);
 }
 
 // Create a prompt asking the purchase quantity and returning an alert if the reply doesn't meet the parameters
 
 function purchase(){
-    console.log(typeof(purchaseOrder))
-    var purchaseOrder = prompt('How many would you like to purchase?','1-5');
-        while (purchaseOrder === null || purchaseOrder === '0') {
-            purchaseOrder = confirm('You haven\'t made a purchase yet.');
-        }
-        for (purchaseOrder > 0, purchaseOrder <= 5)
-            purchaseOrder = confirm('Thank you for your purchase!');
-        }
+
+    console.log(typeof(purchaseOrder));
+
+    var purchaseOrder = prompt('How many would you like to purchase? 1-5');
+    while (purchaseOrder === '0') {
+            confirm('You haven\'t made a purchase yet.');
+            purchaseOrder = prompt('How many would you like to purchase? 1-5');
     }
+    while (purchaseOrder >5) {
+        confirm('Due to high demand, we\'re only allowing up to 5 purchases.');
+        purchaseOrder = prompt('How many would you like to purchase? 1-5');
+}
+    for (var i = 0; i <= purchaseOrder; i++) {
+            document.write('<img src="https://i2.wp.com/hospitalnews.com/wp-content/uploads/2019/07/iStock-921950686.jpg?w=360&ssl=1">');
+    }
+    
 }
