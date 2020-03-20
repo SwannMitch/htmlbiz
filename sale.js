ac79342fddf7b89b6e58563d910c39184d456951
SwannMitch <swann_m@rocketmail.com>
2020-03-19 17:51:02 -0700

    fixed function

--- sale.js
+++ sale.js
@@ -2,17 +2,21 @@ var today = new Date();
 var hourNow = today.getHours();
 var sale;
 
-if (hourNow > 17) {
-    sale = 'Sale has expired!';
-} else if (hourNow > 12) {
-    sale = 'Sale ends soon!';
-} else if (hourNow > 0) {
-    sale = 'Sale ends this afternoon!';
-} else {
-    sale = 'Sale ends every afternoon!';
-}
 
-document.write('<h3>' + sale + '</h3>');
+function sale(){
+
+    if (hourNow > 17) {
+        sale = 'Sale has expired!';
+    } else if (hourNow > 12) {
+        sale = 'Sale ends soon!';
+    } else if (hourNow > 0) {
+        sale = 'Sale ends this afternoon!';
+    } else {
+        sale = 'Sale ends every afternoon!';
+    }
+
+    document.write('<h3>' + sale + '</h3>');
+}
 
 alert('These statements have not been evaluated by the Food and Drug Administration. These products are not intended to diagnose, treat, cure or prevent any disease.');
 
